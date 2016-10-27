5488d470a39071dad24a17eae4c6bd734187a848
DN <windgotseen@gmail.com>
2016-10-27 20:15:33 +0800

    Tutorial level 1 added sprites, scripts for elements and background

---
+++ Assets/Script/Thepowerfulrock.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thepowerfulrock : MonoBehaviour {
+
+    public int hp = 100;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
